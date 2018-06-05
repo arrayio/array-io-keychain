5c8be660e512cf831bab5d1f4afd3ee466dc5853
sinev-valentine <sinev-valentine@yandex.ru>
2018-06-05 14:30:29 +0300

    secure password entry research

---
+++ app/linux/passentry_gui/main.cpp
@@ -0,0 +1,23 @@
+#include "widget.h"
+#include <QApplication>
+#include <QDesktopWidget>
+
+
+int main(int argc, char *argv[])
+{
+
+    QApplication a(argc, argv);
+    Widget w;
+    const QRect sz = QApplication::desktop()->availableGeometry(&w);
+    w.resize(sz.width() / 4, sz.height() * 1 / 6);
+    w.move((sz.width() - w.width()) / 2,
+                (sz.height() - w.height()) / 2);
+    w.show();
+
+    return a.exec();
+
+}
+
+
+
+
---
+++ app/linux/passentry_gui/widget.cpp
@@ -0,0 +1,51 @@
+#include "widget.h"
+#include <QGridLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QPushButton>
+
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    setWindowTitle(tr("Promt for password"));
+    CreateFormInterior();
+}
+
+Widget::~Widget()
+{}
+
+void Widget::CreateFormInterior()
+{
+    QGridLayout * grid = new QGridLayout(this);
+    {
+        QLabel *plb = new QLabel(tr("input password"), this);
+        grid->addWidget(plb, 0, 0);
+    }
+    {
+        QLineEdit *ple = new QLineEdit(this);
+        ple->setEchoMode(QLineEdit::Password);
+        grid->addWidget(ple, 0, 1, 1, 3);
+    }
+    {
+        QLabel *plb = new QLabel(tr("raw transaction"), this);
+        grid->addWidget(plb, 1, 0);
+    }
+    {
+        QTextEdit * pte = new QTextEdit(this);
+        grid->addWidget(pte, 1, 1, 2, 3);
+        pte->setText("всякаяхернявсякаяхернявсякаяхернявсякаяхернявсякаяхернявсякаяхернявсякаяхерня");
+        pte->setReadOnly(true);
+    }
+    {
+        QPushButton *plb = new QPushButton("Ok", this);
+        grid->addWidget(plb, 3, 1);
+        connect(plb, SIGNAL(clicked()), this, SLOT(close()));
+    }
+    {
+        QPushButton * ppb = new QPushButton("Cancel", this);
+        grid->addWidget(ppb, 3, 2);
+        connect(ppb, SIGNAL(clicked()), this, SLOT(close()));
+    }
+}

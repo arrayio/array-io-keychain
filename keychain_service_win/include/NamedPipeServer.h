27e4148a040f3c6ce574d82cade0b9ab1ec906e8
roman <ibr.roman@gmail.com>
2018-06-09 16:09:04 +0300

    add CMakeLists.txt for windows service

---
+++ keychain_service_win/include/NamedPipeServer.h
@@ -0,0 +1,26 @@
+#ifndef _NAMED_PIPE_SERVER_H
+#define _NAMED_PIPE_SERVER_H
+#include <windows.h> 
+#include <stdio.h> 
+#include <tchar.h>
+#include <strsafe.h>
+
+#define BUFSIZE 512
+
+static DWORD WINAPI InstanceThread(LPVOID lpvParam);
+static VOID GetAnswerToRequest(LPTSTR pchRequest, LPTSTR pchReply, LPDWORD pchBytes);
+
+class NamedPipeServer {
+public: 
+	NamedPipeServer();
+	~NamedPipeServer();
+	void ListenChannel();
+	void ListenPasswdSecureChannel();
+private:
+	BOOL fConnected = FALSE;
+	DWORD  dwThreadId = 0;
+	HANDLE hPipe = INVALID_HANDLE_VALUE;
+	HANDLE hThread = NULL;
+	LPTSTR lpszPipename;
+};
+#endif

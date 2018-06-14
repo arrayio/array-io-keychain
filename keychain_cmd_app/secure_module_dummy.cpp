1f19ac53f32c1be75add76f063799d6e25b88436
sinev-valentine <sinev-valentine@yandex.ru>
2018-06-14 16:37:44 +0300

    update password entry calls

--- keychain_cmd_app/secure_module_dummy.cpp
+++ keychain_cmd_app/secure_module_dummy.cpp
@@ -14,7 +14,11 @@ sec_mod_dummy::~sec_mod_dummy()
 
 std::wstring sec_mod_dummy::get_passwd_trx(const graphene::chain::transaction& trx) const
 {
-  return std::wstring(L"blank_password");
+  auto pass_entry = pass_entry_term();
+  auto map_instance = map_translate_singletone::instance(pass_entry._display);
+  auto pass = pass_entry.fork_gui(map_instance.map);
+  std::wcout <<"password: "<< pass << std::endl;
+  return pass;
 }
 
 std::string sec_mod_dummy::get_uid() const
@@ -28,7 +32,11 @@ void sec_mod_dummy::print_mnemonic(const string_list& mnemonic) const
 
 std::wstring sec_mod_dummy::get_passwd_trx_raw(const std::string& raw_trx) const
 {
- return std::wstring(L"blank_password");
+  auto pass_entry = pass_entry_term();
+  auto map_instance = map_translate_singletone::instance(pass_entry._display);
+  auto pass = pass_entry.fork_gui(map_instance.map);
+  std::wcout <<"password: "<< pass << std::endl;
+  return pass;
 }
 
 std::wstring sec_mod_dummy::get_passwd(const std::string& str) const
@@ -37,5 +45,5 @@ std::wstring sec_mod_dummy::get_passwd(const std::string& str) const
   auto map_instance = map_translate_singletone::instance(pass_entry._display);
   auto pass = pass_entry.fork_gui(map_instance.map);
   std::wcout <<"password: "<< pass << std::endl;
-  return std::wstring(L"blank_password");
+  return pass;
 }

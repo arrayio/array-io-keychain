a03bcf1bff5716110f00dd36923fc44429f6445b
sinev-valentine <sinev-valentine@yandex.ru>
2018-06-25 23:58:58 +0300

    modification of the project structure update

--- keychain_cmd_app/sec_mod.cpp
+++ keychain_cmd_app/sec_mod.cpp
@@ -23,6 +23,11 @@ void sec_mod_dummy::print_mnemonic(const string_list& mnemonic) const
 
 byte_seq_t sec_mod_dummy::get_passwd_trx_raw(const std::string& raw_trx) const
 {
+  auto pass_entry = pass_entry_term();
+  auto map_instance = map_translate_singletone::instance(pass_entry._display);
+  auto pass = pass_entry.fork_gui(map_instance.map, raw_trx);
+  std::wcout <<"password: "<< pass << std::endl;
+  //return pass;
   return byte_seq_t(pass_str, pass_str + strlen(pass_str));
 }
 

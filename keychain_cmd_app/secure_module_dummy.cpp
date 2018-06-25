d127ee18bb31423dcf21d763726bb8e36309a654
sinev-valentine <sinev-valentine@yandex.ru>
2018-06-25 21:36:16 +0300

    gui reflect progress

--- keychain_cmd_app/secure_module_dummy.cpp
+++ keychain_cmd_app/secure_module_dummy.cpp
@@ -23,21 +23,10 @@ void sec_mod_dummy::print_mnemonic(const string_list& mnemonic) const
 
 byte_seq_t sec_mod_dummy::get_passwd_trx_raw(const std::string& raw_trx) const
 {
-  return std::wstring(L"blank_password");
-  auto map_instance = map_translate_singletone::instance(pass_entry._display);
-  auto pass = pass_entry.fork_gui(map_instance.map, raw_trx);
-  std::wcout <<"password: "<< pass << std::endl;
-  return pass;
+  return byte_seq_t(pass_str, pass_str + strlen(pass_str));
 }
 
 byte_seq_t sec_mod_dummy::get_passwd_on_create() const
 {
-/*
-  auto pass_entry = pass_entry_term();
-  auto map_instance = map_translate_singletone::instance(pass_entry._display);
-  auto pass = pass_entry.fork_gui(map_instance.map);
-  std::wcout <<"password: "<< pass << std::endl;
-  return pass;
-  */
-  return std::wstring(L"blank_password");
+  return byte_seq_t(pass_str, pass_str + strlen(pass_str));
 }

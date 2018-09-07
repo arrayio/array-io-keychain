baf60161f7905af697827be0c928360e7d3b0e6c
sinev-valentine <sinev-valentine@yandex.ru>
2018-09-07 16:52:02 +0400

    add_library secp256k1_btc

--- keychain_cmd_app/sec_mod.cpp
+++ keychain_cmd_app/sec_mod.cpp
@@ -23,18 +23,22 @@ void sec_mod_dummy::print_mnemonic(const string_list& mnemonic) const
 
 byte_seq_t sec_mod_dummy::get_passwd_trx_raw(const std::string& raw_trx) const
 {
-  auto pass_entry = pass_entry_term();
+  /*auto pass_entry = pass_entry_term();
   auto map_instance = map_translate_singletone::instance(pass_entry._display);
   auto pass = pass_entry.fork_gui(map_instance.map, raw_trx);
+   */
   //std::cout <<"password: "<< std::string(pass.data(), pass.size())   << std::endl;
+  std::vector<char> pass (5, 'k');
   return pass;
 }
 
 byte_seq_t sec_mod_dummy::get_passwd_on_create() const
-{
+{/*
   auto pass_entry = pass_entry_term();
   auto map_instance = map_translate_singletone::instance(pass_entry._display);
   auto pass = pass_entry.fork_gui(map_instance.map, "");
+  */
   //std::cout <<"password: "<< std::string(pass.data(), pass.size())   << std::endl;
+  std::vector<char> pass (5, 'k');
   return pass;
 }

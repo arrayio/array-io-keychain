0d8cb9592c0cec45506dd93c218a9656327f0d04
loutskiy <mikhail@lwts.ru>
2018-11-26 19:52:56 +0300

    Menu

--- keychain_mac/sec_mod_mac.mm
+++ keychain_mac/sec_mod_mac.mm
@@ -40,13 +40,27 @@ byte_seq_t sec_mod_mac::get_passwd_trx_raw(const std::string& raw_trx, std::stri
 
 byte_seq_t sec_mod_mac::get_passwd_on_create(std::string binary_dir) const
 {
-    [NSApplication sharedApplication];
-    [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-    [NSApp activateIgnoringOtherApps:YES];
-    
-    NSRect frame = NSMakeRect(0, 0, 575, 361);
-    MyDialog *dialog = [[MyDialog alloc] initWithFrame:frame];
-    [dialog runModal];
+    @autoreleasepool {
+        [NSApplication sharedApplication];
+        [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+        id menubar = [NSMenu new];
+        id appMenuItem = [NSMenuItem new];
+        [menubar addItem:appMenuItem];
+        [NSApp setMainMenu:menubar];
+        id appMenu = [NSMenu new];
+        id appName = [[NSProcessInfo processInfo] processName];
+        id quitTitle = @"Quit Keychain";
+        id quitMenuItem = [[NSMenuItem alloc] initWithTitle:quitTitle
+                                                      action:@selector(terminate:) keyEquivalent:@"q"];
+        [appMenu addItem:quitMenuItem];
+        [appMenuItem setSubmenu:appMenu];
+        
+        [NSApp activateIgnoringOtherApps:YES];
+        [NSApp finishLaunching];
+        NSRect frame = NSMakeRect(0, 0, 575, 361);
+        MyDialog *dialog = [[MyDialog alloc] initWithFrame:frame];
+        [dialog runModal];
+    }
     std::string str = std::string([[[PassSyncStore sharedInstance] pass] UTF8String]);
     [[PassSyncStore sharedInstance] setPass:@""];
     keychain_app::byte_seq_t pass(str.begin(), str.end());

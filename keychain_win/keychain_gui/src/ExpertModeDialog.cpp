afdbe69bfecd8d4097671a554e63a6251df02526
Ilia Puts <iliaputs@gmail.com>
2018-12-12 12:24:31 +0500

    extend ExpertModeDialog

--- keychain_win/keychain_gui/src/ExpertModeDialog.cpp
+++ keychain_win/keychain_gui/src/ExpertModeDialog.cpp
@@ -4,16 +4,16 @@ ExpertModeDialog::ExpertModeDialog(QWidget * parent)
 	: QDialog(parent)
 {
 	QMetaObject::connectSlotsByName(this);
-	setFixedSize(400, 300);
+	setFixedSize(600, 400);
 	setWindowFlags(Qt::FramelessWindowHint);
 	setStyleSheet("background-color:rgb(227,232,248);border-style:outset;border-width:0px;border-radius:5px");
 	transactionDescription = new QPlainTextEdit(this);
 	transactionDescription->setStyleSheet("color:rgb(70,134,255);background:transparent;border-style:outset;border-width:0px;border-radius:5px;font:15px \"Segoe UI\"");
-	transactionDescription->setFixedSize(380, 240);
+	transactionDescription->setFixedSize(580, 340);
 	transactionDescription->move(5,5);
 	OKButton = new QPushButton(this);
 	OKButton->setFixedSize(100,25);
-	OKButton->move(150, 250);
+	OKButton->move(250, 350);
 	OKButton->setCursor(Qt::PointingHandCursor);
 	OKButton->setText("OK");
 	OKButton->setStyleSheet("color:white;background-color:rgb(70,134,255);border-style:outset;border-width:0px;border-radius:5px;font:16px \"Segoe UI\"");

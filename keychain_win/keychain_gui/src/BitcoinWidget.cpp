ba0c108acdbbd023875d6ab221f348810d7af3d7
Ilia Puts <iliaputs@gmail.com>
2018-12-12 12:25:14 +0500

    add multiple from items
    check from items is <15, if more show extend button

--- keychain_win/keychain_gui/src/BitcoinWidget.cpp
+++ keychain_win/keychain_gui/src/BitcoinWidget.cpp
@@ -34,29 +34,53 @@ BitcoinWidget::BitcoinWidget(Transaction &transaction, QWidget * parent)
 	auto bitcoin_trx = cmd_parse.to_bitcoin();
 	auto bitcoin_data = bitcoin_trx.trx_info;
 	
-	auto num_vouts = bitcoin_data.num_vouts;//chech num of vouts
-	
+	num_vouts = bitcoin_data.num_vouts;//chech num of vouts
+	bool overflow = false;
+	if (num_vouts > 15) {
+		num_vouts = 15;
+		overflow = true;
+	}
+
 	if (bitcoin_data.num_vouts >= 1) {
-		auto vout1 = bitcoin_data.vouts[0];
+		//auto vout1 = bitcoin_data.vouts[0];
 		from = new SecureWindowElement(this);
 		from->SetLabelStyle(labelStyle);
 		from->SetValueStyle(valueStyle);
 		from->SetLabelAndValue("From", QString::fromStdString(bitcoin_trx.from));
 
-		to = new SecureWindowElement(this);
-		to->SetLabelStyle(labelStyle);
-		to->SetValueStyle(valueStyle);
-		to->SetLabelAndValue("To", QString::fromStdString(vout1.address));
+		to = new SecureWindowElement*[num_vouts];
+		amount = new SecureWindowElement*[num_vouts];
+		
+		for (int i = 0; i < num_vouts; i++) {
+			auto vout1 = bitcoin_data.vouts[i];
+			*(to + i) = new SecureWindowElement(this);
+			(*(to + i))->SetLabelStyle(labelStyle);
+			(*(to + i))->SetValueStyle(valueStyle);
+			(*(to + i))->SetLabelAndValue("To", QString::fromStdString(vout1.address));
 
-		amount = new SecureWindowElement(this);
-		amount->SetLabelStyle(labelStyle);
-		amount->SetValueStyle(valueStyle);
-		amount->SetLabelAndValue("Amount", QString::number(vout1.amount) + " BTC");
+			*(amount+i) = new SecureWindowElement(this);
+			(*(amount + i))->SetLabelStyle(labelStyle);
+			(*(amount + i))->SetValueStyle(valueStyle);
+			(*(amount + i))->SetLabelAndValue("Amount", QString::number(vout1.amount) + " BTC");
+		}
 	}
 
+	if (cmd_parse.unlock_time() > 0) {
+		unlockTime = new PrivateKeyInMemory(this);
+		unlockTime->SetTime(QString::number(cmd_parse.unlock_time()));
+	}
+
+	if (overflow) {
+		lookAll = new QPushButton(this);
+		lookAll->setFixedSize(13, 7);
+		lookAll->setFlat(true);
+		lookAll->setStyleSheet("background-image:url(:/keychain_gui_win/more_arrow.png);border-style:outset;border-width:0px;");
+		lookAll->setWindowFlags(Qt::FramelessWindowHint);
+		lookAll->setCursor(Qt::PointingHandCursor);
+		connect(lookAll, &QPushButton::clicked, this, &BitcoinWidget::lookUpAll);
+	}
 	expertModeElement = new ExpertModeElement(this);
 	expertModeElement->SetExpertModeText(transaction.getTransactionText());
-
 }
 
 void BitcoinWidget::SetPosition(int x, int y, int width)
@@ -67,12 +91,23 @@ void BitcoinWidget::SetPosition(int x, int y, int width)
 	from->SetPosition(0, currentHeight, 116, width);
 	from->move(0, currentHeight);
 	currentHeight += 26;
-	to->SetPosition(0, currentHeight, 116, width*0.75);
-	to->move(0, currentHeight);
-	amount->SetPosition(to->width(), currentHeight, 116, width/2);
-	amount->move(to->width(), currentHeight);
-	currentWidth = to->width() + amount->width();
-	currentHeight += 26;
+	
+	for (int i = 0; i < num_vouts; i++) {
+		(*(to+i))->SetPosition(0, currentHeight, 116, width*0.75);
+		(*(to+i))->move(0, currentHeight);
+		(*(amount+i))->SetPosition((*(to+i))->width(), currentHeight, 116, width / 3);
+		(*(amount+i))->move((*(to+i))->width(), currentHeight);
+		currentWidth = (*(to+i))->width() + (*(amount+i))->width();
+		currentHeight += 26;
+	}
+	if (lookAll != Q_NULLPTR) {
+		lookAll->move(currentWidth-40, currentHeight-10);
+	}
+	if (unlockTime != Q_NULLPTR) {
+		unlockTime->SetPosition(0, currentHeight, 116, width);
+		unlockTime->move(0, currentHeight);
+		currentHeight += 36;
+	}
 	expertModeElement->SetPosition(0, currentHeight, 116, width);
 	expertModeElement->move(0, currentHeight);
 	currentHeight += 60;
@@ -86,6 +121,10 @@ int BitcoinWidget::GetCurrentHeight() {
 
 int BitcoinWidget::GetCurrentWidth() {
 	return currentWidth;
+}
+
+void BitcoinWidget::lookUpAll() {
+
 }
 
 BitcoinWidget::~BitcoinWidget()

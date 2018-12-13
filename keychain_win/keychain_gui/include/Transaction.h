64a75d82700ea5e2a0a77f24a4d3d0c5e4796f70
Ilia Puts <iliaputs@gmail.com>
2018-12-13 10:40:46 +0500

    add unlockTime and isPassword methods to Transaction class

--- keychain_win/keychain_gui/include/Transaction.h
+++ keychain_win/keychain_gui/include/Transaction.h
@@ -13,8 +13,16 @@ class Transaction {
 public:
 	Transaction(QString &transaction);
 	QString getTransactionText();
+	bool isCreatePassword();
+	int isUnlockKey();
+	QString unlockKeyName();
+	void setCreatePassword();
+	void setUnlockKey(QString &key_name, int unlock_time);
 private:
 	QString sourceTransaction;
+	int unlock_time =-1;
+	QString key_name = Q_NULLPTR;
+	bool createPassword = false;
 };
 
 

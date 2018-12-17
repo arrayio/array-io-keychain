9911eff7ff45297d305887c0f073fc425fdd1b99
sinev-valentine <sinev-valentine@yandex.ru>
2018-12-17 12:05:31 +0300

    ws_daemon_wrap update

--- ws/main.cpp
+++ ws/main.cpp
@@ -1,76 +1,27 @@
 #include <iostream>
 #include <unistd.h>
-#include <fstream>
 #include <sys/wait.h>
+#include <pwd.h>
+#include <grp.h>
 
-#define get_uid "id -u keychain_service"
-#define get_gid "getent group input | awk -F: '{printf $3}'"
-#define xhost "xhost +SI:localuser:keychain_service"
+#define user "keychain_service"
+#define xhost "xhost +SI:localuser:" user
 #define websocketd "/home/user/ws/websocketd"
 #define keychain "/home/user/ws/keychain"
 
 
 int main() {
 
-    int pfd[2], res;
-    uid_t uid = 0 , gid;
-    char buf[1024];
-    std::string s;
-
-    if (pipe(pfd) == -1)  throw std::runtime_error("pipe() 1");
-    switch (fork())
-    {
-        case (-1): throw std::runtime_error("fork() 1");
-        case 0:
-        {
-            if (close(pfd[0]) == -1) throw std::runtime_error("close child(0) 1");
-            if (pfd[1] != STDOUT_FILENO) {
-                if (dup2(pfd[1], STDOUT_FILENO) == -1) throw std::runtime_error("dup2 1");
-                if (close(pfd[1]) == -1) throw std::runtime_error("close child(1) 1");
-            }
-            std::system(get_uid);
-            _exit(EXIT_SUCCESS);
-        }
-        default: break;
-    }
-    if (close(pfd[1]) == -1)  throw std::runtime_error("close parent(1) 1");
-
-    res = read(pfd[0], &buf, sizeof(buf));
-    s = std::string(buf, buf+res);
-    uid = std::stoi(s);
-    if (close(pfd[0]) == -1) throw std::runtime_error("close parent(0) 1");
-
-    if (pipe(pfd) == -1)  throw std::runtime_error("pipe() 2");
-    switch (fork())
-    {
-        case (-1): throw std::runtime_error("fork() 2");
-        case 0:
-        {
-            if (close(pfd[0]) == -1) throw std::runtime_error("close child(0) 2");
-            if (pfd[1] != STDOUT_FILENO) {
-                if (dup2(pfd[1], STDOUT_FILENO) == -1) throw std::runtime_error("dup2 2");
-                if (close(pfd[1]) == -1) throw std::runtime_error("close child(1) 2");
-            }
-            std::system(get_gid);
-            _exit(EXIT_SUCCESS);
-        }
-        default: break;
-    }
-    if (close(pfd[1]) == -1)  throw std::runtime_error("close parent(1) 2");
-
-    res = read(pfd[0], &buf, sizeof(buf));
-    s = std::string(buf, buf+res);
-    gid = std::stoi(s);
-    if (close(pfd[0]) == -1) throw std::runtime_error("close parent(0) 2");
-
+    struct passwd* pw = getpwnam(user);
     std::system (xhost);
     switch (fork())
     {
-        case (-1): throw std::runtime_error("fork() 3");
+        case (-1): throw std::runtime_error("fork()");
         case 0:
         {
-            if (setregid(gid, gid) == -1) throw std::runtime_error("setregid()");
-            if (setresuid(uid, uid, uid) == -1) throw std::runtime_error("setresuid()");
+            initgroups(user,pw->pw_gid);
+            if (setgid(pw->pw_gid) == -1) throw std::runtime_error("setgid()");
+            if (setuid(pw->pw_uid) == -1) throw std::runtime_error("setuid()");
             execlp( websocketd,
                     websocketd,
                     "--port=8080", "--devconsole", "--passenv=DISPLAY", keychain,

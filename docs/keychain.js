f2fa4e5c7cba76cf33af02fa470263a86a6ef1b9
Dmitry Taipov <dtaipov@gmail.com>
2018-12-26 01:17:12 +0300

    #85 singTransaction() two steps logic moved to one function

--- docs/keychain.js
+++ docs/keychain.js
@@ -35,15 +35,45 @@
     return this.method({command: 'sign_hex',  params });
   };
 
-  WS.prototype.signTransaction = function(tx, privateKey) {
-    const rsv = { r: '0x00', s: '0x00', v: tx.chainId };
+  WS.prototype.signTransaction = function (tx, privateKey) {
+    tx.gasLimit = tx.gas; // rename gas to gasLimit for using with new EthJS.Tx()
+    const rsv = this.rsv('', tx.chainId);
+    const rawHex = this.getRawHex(rsv, tx);
+
+    const params = { keyname: privateKey, transaction: rawHex, blockchain_type: "ethereum" };
+    return this.method({ command: 'sign_hex', params })
+      .then(data => {
+        const signature = data.result;
+        const rsv = this.rsv(signature, tx.chainId);
+        return this.getRawHex(rsv, tx);
+      });
+  };
+
+  WS.prototype.getRawHex = function(rsv, tx) {
     const txParams = {...tx, ...rsv};
     const ethTx = new EthJS.Tx(txParams);
     const buffer = ethTx.serialize();
-    const rawHex = buffer.toString('hex');
+    return buffer.toString('hex');
+  }
 
-    const params = { keyname: privateKey, transaction: rawHex, blockchain_type: "ethereum" };
-    return this.method({command: 'sign_hex',  params });
+
+  WS.prototype.rsv = function(signature, chainId) {
+    const ret = {};
+    if (signature) {
+      ret.r = `0x${signature.slice(0, 64)}`;
+      ret.s = `0x${signature.slice(64, 128)}`;
+      const recovery = parseInt(signature.slice(128, 130), 16);
+      let tmpV = recovery + 27;
+      if (chainId > 0) {
+        tmpV += chainId * 2 + 8;
+      }
+      ret.v = tmpV;
+    } else {
+      ret.r = '0x00';
+      ret.s = '0x00';
+      ret.v = chainId;
+    }
+    return ret;
   };
 
 }).call(this);

dbd09ce9b04f1c2b575df2fb58811f191522170d
NuclearRazor <nuclearrazor@gmail.com>
2019-02-07 22:22:19 +0300

    #115 implement qt logging redirection function

--- keychain_keymanager/src/main.cpp
+++ keychain_keymanager/src/main.cpp
@@ -3,6 +3,7 @@
 #include <QJsonDocument>
 //#include "Transaction.h"
 #include <QString>
+#include <QtGlobal>
 #include <cwchar>
 #include <keychain_lib/keychain_logger.hpp>
 #include <keychain_lib/secmod_parser_cmd.hpp>
@@ -10,7 +11,35 @@
 using namespace std;
 using namespace keychain_app;
 
+namespace attrs = boost::log::attributes;
+
 using secmod_commands::secmod_parser_f;
+
+static const QtMessageHandler QT_DEFAULT_MESSAGE_HANDLER = qInstallMessageHandler(0);
+
+void HandleLoggingOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    auto log = logger_singleton::instance();
+    switch (type) {
+    case QtDebugMsg:
+        BOOST_LOG_SEV(log.lg, debug) << "Debug: %s (%s:%u, %s)\n" << localMsg.constData() << context.file << context.line << context.function;
+        break;
+    case QtInfoMsg:
+        BOOST_LOG_SEV(log.lg, info) << "Info: %s (%s:%u, %s)\n" << localMsg.constData() << context.file << context.line << context.function;
+        break;
+    case QtWarningMsg:
+        BOOST_LOG_SEV(log.lg, warning) << "Warning: %s (%s:%u, %s)\n" << localMsg.constData() << context.file << context.line << context.function;
+        break;
+    case QtCriticalMsg:
+        BOOST_LOG_SEV(log.lg, error) << "Critical: %s (%s:%u, %s)\n" << localMsg.constData() << context.file << context.line << context.function;
+        break;
+    case QtFatalMsg:
+        BOOST_LOG_SEV(log.lg, fatal) << "Fatal: %s (%s:%u, %s)\n" << localMsg.constData() << context.file << context.line << context.function;
+        abort();
+    }
+    (*QT_DEFAULT_MESSAGE_HANDLER)(type, context, msg);
+}
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +47,9 @@ int main(int argc, char *argv[])
 	keymanager_dialog manager;
 	manager.init();
 	manager.show();
+
+    //set logging redirection function
+    qInstallMessageHandler(HandleLoggingOutput);
 
 	a.exec();
 	return 0;
